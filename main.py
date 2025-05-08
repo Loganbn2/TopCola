855fb42bc34fb93264087a12fd190de6766f827f
Logan Nicholson <logan@localeyes.guide>
2025-05-08 17:23:40 -0600

    testing supabase

--- main.py
+++ main.py
@@ -12,19 +12,27 @@ app = Flask(__name__)
 CORS(app)
 
 # Initialize Supabase client
-SUPABASE_URL = "https://otnrvaybkwsvzxgwfhfc.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90bnJ2YXlia3dzdnp4Z3dmaGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MjYwMDIsImV4cCI6MjA2MjMwMjAwMn0.Ox3qDjoX-x-Tcol96i1XQRuqRWr8oUkc_Z-pZaBJTww"
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+import os
+from supabase import create_client, Client
 
-def calculate():
-    return random.randint(1, 10)
+url: str = os.environ.get("https://otnrvaybkwsvzxgwfhfc.supabase.co")
+key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90bnJ2YXlia3dzdnp4Z3dmaGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MjYwMDIsImV4cCI6MjA2MjMwMjAwMn0.Ox3qDjoX-x-Tcol96i1XQRuqRWr8oUkc_Z-pZaBJTww")
+supabase: Client = create_client(url, key)
+
+def fetch_supabase_products():
+    (supabase.table("Products")
+    .select("Product Name")
+    .execute()
+)
 
 @app.route('/product-list')  # Change the route to match how the template is accessed
 def get_product_list():
-    value = calculate()  # Dynamically generate the value
+    value = fetch_supabase_products()  # Dynamically generate the value
     return render_template('product_list.html', value=value)  # Pass value to the template
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    #app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
+
 
 
