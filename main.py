92f057b14a900605d927d18186cfa478890aded2
Logan Nicholson <logan@localeyes.guide>
2025-05-08 10:48:40 -0600

    Testing html

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify
+from datetime import datetime  # Import datetime module
 
 app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
     return jsonify({"message": "Hello from the backend!"})
+
+@app.route('/api/datetime', methods=['GET'])
+def get_datetime():
+    # Function to get the current date and time
+    now = datetime.now()
+    return jsonify({"current_datetime": now.strftime("%Y-%m-%d %H:%M:%S")})
 
 if __name__ == '__main__':
     app.run(debug=True)
