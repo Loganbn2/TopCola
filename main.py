0e047ddba1edc70d8acc80d282d33a50b66e7272
Logan Nicholson <logan@localeyes.guide>
2025-05-08 15:55:25 -0600

    test

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 from flask import Flask, jsonify
 from datetime import datetime  # Import datetime module
 from supabase import create_client, Client  # Import Supabase client
-import random  # Import random module
+
 
 # Initialize Supabase client
 # Replace with your real Supabase credentials
 SUPABASE_URL = "https://otnrvaybkwsvzxgwfhfc.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90bnJ2YXlia3dzdnp4Z3dmaGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MjYwMDIsImV4cCI6MjA2MjMwMjAwMn0.Ox3qDjoX-x-Tcol96i1XQRuqRWr8oUkc_Z-pZaBJTww"
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-app = Flask(__name__)
-
-from flask_cors import CORS
-CORS(app)  # Allow all origins (not secure for production)
-
-@app.route('/api/random-number', methods=['GET'])  # Use relative path
-def get_random_number():
-    return jsonify({"random_number": random.randint(1, 10)})
-
-
-if __name__ == '__main__':
-    import os
-    # Use Render's environment variables for deployment
-    app.run()
